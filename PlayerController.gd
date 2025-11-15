c09a92ef779de7d3edec3b415acbeac55409a45e
Andrew Teske <50252390+pudgypoultry@users.noreply.github.com>
2025-11-15 13:15:20 -0800

    small change to player and enemy managers

--- PlayerController.gd
+++ PlayerController.gd
@@ -1,13 +1,20 @@
 extends Node3D
 
+@export_category("Plugging In Nodes")
+
+@export_category("Game Rules")
 @export var throttle : float = 1.0
 @export var throttle_min : float = 0.0
 @export var throttle_max : float = 10.0
-@export var direction : Vector3
-@export var playerHealth : float = 100.0
-@export var rotationSpeed : float = 0.1
+@export var player_health : float = 100.0
+@export var nose_rotation_speed : float = 1.0
+@export var roll_rotation_speed : float = 1.0
 
+var direction : Vector3
 var canAct : bool = true
+
+var equipped_weapons = []
+var equipped_passives = []
 
 
 # Called when the node enters the scene tree for the first time.
@@ -22,9 +29,9 @@ func _process(delta: float) -> void:
 	var throttle_change = Input.get_axis("throttle_down", "throttle_up")
 	
 	if up_down != 0:
-		rotation += transform.basis.x * up_down * delta
+		rotation += transform.basis.x * up_down * nose_rotation_speed * delta
 	if left_right != 0:
-		rotation += transform.basis.z * left_right * delta
+		rotation += transform.basis.z * left_right * roll_rotation_speed * delta
 	if throttle_change != 0:
 		throttle += throttle_change * delta
 	
--- Scenes/enemy_manager.gd
+++ Scenes/enemy_manager.gd
@@ -3,3 +3,9 @@ extends Node
 # hold enemies, types, etc.
 
 # manage timers, spawning, stopping on menus
+
+# function to run through list of enemies and pause them
+
+# function to run through list of enemies and damage them
+
+# function to run through list of enemies and destroy selected types
