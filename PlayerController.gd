31caffc0e6f7ca0756ab7521ab55a71170f511a6
Andrew Teske <50252390+pudgypoultry@users.noreply.github.com>
2025-11-15 13:32:53 -0800

    fixed rotation of player

--- PlayerController.gd
+++ PlayerController.gd
@@ -4,6 +4,7 @@ extends Node3D
 
 @export_category("Game Rules")
 @export var throttle : float = 1.0
+@export var throttle_change_rate : float = 1.0
 @export var throttle_min : float = 0.0
 @export var throttle_max : float = 10.0
 @export var player_health : float = 100.0
@@ -29,11 +30,14 @@ func _process(delta: float) -> void:
 	var throttle_change = Input.get_axis("throttle_down", "throttle_up")
 	
 	if up_down != 0:
-		rotation += transform.basis.x * up_down * nose_rotation_speed * delta
+		
+		rotate_object_local(Vector3.RIGHT, up_down * nose_rotation_speed * delta)
+		#rotation += transform.basis.x * up_down * nose_rotation_speed * delta
 	if left_right != 0:
-		rotation += transform.basis.z * left_right * roll_rotation_speed * delta
+		rotate_object_local(Vector3.FORWARD, left_right * roll_rotation_speed * delta)
+		#rotation += transform.basis.z * left_right * roll_rotation_speed * delta
 	if throttle_change != 0:
-		throttle += throttle_change * delta
+		throttle += throttle_change * throttle_change_rate * delta
 	
 	clampf(throttle, throttle_min, throttle_max)
 	
