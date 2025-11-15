24df617ec9484131e981ab7d553d1b912b4305d7
bbkkpluspplus <austinjgerardo@gmail.com>
2025-11-15 13:47:26 -0800

    start/quit buttons work
    Click to use buttons to change scene

--- control.gd
+++ control.gd
@@ -1,9 +1,11 @@
 extends Control
+@export var mainscene = "res://sandbox.tscn"
 
 
 
 
 func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file(mainscene)
 	pass # Replace with function body.
 	
 
