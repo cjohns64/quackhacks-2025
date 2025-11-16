a35443d567f53bd5f21af95e8e7990cd2338469a
coleherman43 <colesten@gmail.com>
2025-11-16 11:49:37 -0800

    Level up system

--- Scenes/levellabel.gd
+++ Scenes/levellabel.gd
@@ -1,7 +1,12 @@
 extends Label
 
-
+func level_up():
+	var levelup = $"../../Levelup"
+	Globallevelscript.level += 1
+	levelup.show_level_up_menu()
 
 func _process(delta):
 	# Access the global score and set the label's text
 	text = "Level: " + str(Globallevelscript.level)
+	if Globallevelscript.level * 100 < Globalpointscript.score:
+		level_up()
