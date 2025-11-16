2c6589f52db8c36ff429b571be9d91965936182a
Cory Johns <cory@triplevisiongroup.com>
2025-11-16 11:10:55 -0800

    Sword has random rotation

--- Scenes/Weapons/weapon_sword.gd
+++ Scenes/Weapons/weapon_sword.gd
@@ -2,8 +2,11 @@ extends Node3D
 
 var frequency:float = 70
 	
+func _ready() -> void:
+	self.rotation.z = randf() * PI
+
 func _physics_process(delta: float) -> void:
-	self.rotation.y += delta * frequency * PI / 180
+	self.rotate(self.transform.basis.y, delta * frequency * PI / 180)
 
 
 func _on_rigid_body_3d_body_entered(body: Node) -> void:
