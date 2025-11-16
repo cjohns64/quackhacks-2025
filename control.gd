d0e153f528e7ce89a7f93ebd426c31aa91136dca
bbkkpluspplus <austinjgerardo@gmail.com>
2025-11-15 19:18:25 -0800

    pause menu WORKING
    wurk it

--- control.gd
+++ control.gd
@@ -4,14 +4,15 @@ extends Control
 
 
 
+
 func _ready():
-	get_tree().paused= false
+	get_tree().paused= true
 	
 
 
 func _on_button_pressed() ->  void:
 	
-	
+	get_tree().paused= false
 	self.visible= false
 	$click.play()
 	
